c58d0e5aba923cce04ba589138710bf9a5643cf7
Igor Laevsky <igmyrj@gmail.com>
2019-02-10 13:39:30 +0300

    Initial structure

---
+++ cdl/project.clj
@@ -0,0 +1,5 @@
+(defproject ames/cdl "0.1.0"
+  :description ""
+  :url "https://github.com/igor-laevsky/ames"
+  :license {}
+  :dependencies [[org.clojure/clojure "1.10.0"]])
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject ames "0.1.0"
+  :description ""
+  :url "https://github.com/igor-laevsky/ames"
+  :license {}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [ames/cdl "0.1.0"]]
+  :sub ["cdl"])
