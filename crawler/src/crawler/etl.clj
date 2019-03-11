f085436d7442ee1b572a140909a325ab23aeccbd
Igor Laevsky <igmyrj@gmail.com>
2019-03-11 22:25:10 +0300

    Add logout call and test it

--- crawler/src/crawler/etl.clj
+++ crawler/src/crawler/etl.clj
@@ -32,8 +32,27 @@
 (defn- keep-session-url [{{:keys [main-url]} :params}]
   (str main-url "keepsession.aspx?rnd=" (rand)))
 
+(defn subject-matrix [{:keys [network] :as etl}]
+  (net/get network (subject-matrix-url etl)))
+
+;; Helper function to perform RCP call .net style.
+;; Blocks caller while waiting for the response. Returns non-modified server
+;; response map as per clj-http. There is no way to check if server had received
+;; the callback so this will silently not work in case of any errors.
+(defn- call-server-menu! [etl name]
+  (let [view-state (some-> (subject-matrix etl)
+                           (a/<!!)
+                           (:body)
+                           (extr/extract-view-state))
+        args (merge view-state {"__CALLBACKID" "ctl00"
+                                "__CALLBACKPARAM" name})]
+    (-> (net/post (:network etl)
+                  (subject-matrix-url etl)
+                  {:form-params args})
+        (a/<!!))))
+
 ;; Login into application. Blocks caller thread while waiting for the response.
-;; Return true on success false on failure.
+;; Returns true on success false on failure.
 (defn login! [{{:keys [login password]} :params :as etl}]
   (let [login-data-format
         "{login:\"%s\", password:\"%s\", localTime:\"%s UTC +0300\",
@@ -58,26 +77,11 @@
       :ok true
       :has-user (= (parse-response (try-login)) :ok))))
 
-(defn subject-matrix [{:keys [network] :as etl}]
-  (net/get network (subject-matrix-url etl)))
-
-;; RCP call .net style. Blocks caller while waiting for the response.
-;; Returns non-modified server response map as per clj-http.
-;; There is no way to check if server had received the callback so this will
-;; silently not work in case of any errors.
-(defn call-server-menu! [etl name]
-  (let [view-state (some-> (subject-matrix etl)
-                           (a/<!!)
-                           (:body)
-                           (extr/extract-view-state))
-        args (merge view-state {"__CALLBACKID" "ctl00"
-                                "__CALLBACKPARAM" name})]
-    (-> (net/post (:network etl)
-                  (subject-matrix-url etl)
-                  {:form-params args})
-        (a/<!!))))
-
-;; Requests server to continue current user session. Return promise chanel.
+;; Requests server to continue current user session. Doesn't block.
+;; Returns promise chanel with the network response.
 (defn keep-session [etl]
   (net/get (:network etl) (keep-session-url etl)))
 
+;; Logout current user. Blocks caller thread.
+(defn logout! [etl]
+  (call-server-menu! etl "logout"))
--- crawler/test/crawler/test/etl.clj
+++ crawler/test/crawler/test/etl.clj
@@ -1,2 +1,37 @@
 (ns crawler.test.etl
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
+            [clojure.core.async :as a]
+            [com.stuartsierra.component :as component]
+
+            [crawler.etl :as etl]
+            [crawler.network :as network]
+            [crawler.saver :as saver]))
+
+(def creds (read-string (slurp (io/reader (io/resource "creds.edn")))))
+
+(defn create-test-system []
+  (-> (component/system-map
+        :network (network/make-network {:num-threads 10
+                                        :rate        10})
+        :saver (saver/make-file-saver {:file-name "test/resources/saver/tmp.json"})
+        :etl (component/using
+               (etl/make-etl creds)
+               [:network :saver]))))
+
+(deftest test-login-logout
+  (let [{:keys [etl] :as system} (-> (create-test-system) (component/start))]
+    (try
+      ; Can't get system map before we login
+      (is (= 302 (-> (etl/subject-matrix etl) (a/<!!) (:status))))
+      ; Should be able to login
+      (is (= true (etl/login! etl)))
+      ; Login is idempotent
+      (is (= true (etl/login! etl)))
+      ; Now we can get subject matrix
+      (is (= 200 (-> (etl/subject-matrix etl) (a/<!!) (:status))))
+      ; Should be able to logout
+      (is (= 200 (-> (etl/logout! etl) (:status))))
+      ; No subject matrix now
+      (is (= 302 (-> (etl/subject-matrix etl) (a/<!!) (:status))))
+      (finally (component/stop system)))))
