3e72551e53aa757c0d46093e860c07648ee174ae
Igor Laevsky <igmyrj@gmail.com>
2019-07-07 13:42:52 +0300

    Add main function and support hot reload on simple example

--- ui/src/ui/core.cljs
+++ ui/src/ui/core.cljs
@@ -1,3 +1,17 @@
-(ns ui.core)
+(ns ^:figwheel-hooks ui.core
+  (:require [reagent.core :as reagent]
+            [re-frame.core :as re-frame]))
 
-(js/console.log "Hello !")
+(defn example-view []
+  [:h1 "Example wold !"])
+
+(defn mount []
+  (reagent/render [example-view] (.getElementById js/document "app")))
+
+(defn ^:export main []
+  (mount))
+
+(defn ^:after-load re-render []
+  (enable-console-print!)
+  (re-frame/clear-subscription-cache!)
+  (mount))
